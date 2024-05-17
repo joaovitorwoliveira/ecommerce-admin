8a098a2ae1c8a08c7ad53967c745b370680a82c4
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-05-17 16:15:17 -0300

    09 - Categories Entity

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { CategoryColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface CategoryClientProps {
+  data: CategoryColumn[];
+}
+
+export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Categorias (${data.length})`}
+          description="Administre categorias para a sua loja."
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Adicionar Novo
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="Chamadas da API para Categorias" />
+      <Separator />
+      <ApiList entityName="categories" entityIdName="categories" />
+    </>
+  );
+};
