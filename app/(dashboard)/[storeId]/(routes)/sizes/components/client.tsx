a21fa7b339278401eac67e33c34f1f3ea472b102
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-05-20 15:03:03 -0300

    10- Sizes Entity

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { SizeColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface SizesClientProps {
+  data: SizeColumn[];
+}
+
+export const SizesClient: React.FC<SizesClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Tamanhos (${data.length})`}
+          description="Administre tamanhos para a sua loja."
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Adicionar Novo
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="Chamadas da API para Tamanhos" />
+      <Separator />
+      <ApiList entityName="sizes" entityIdName="sizeId" />
+    </>
+  );
+};
